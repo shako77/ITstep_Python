844c281dce4a738e0fd12f642c2946df474f75ab
shako77 <153628699+shako77@users.noreply.github.com>
2024-04-16 19:41:55 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,52 @@
+# 1.
+# score = int(input("შეიყვანეთ თქვენი ქულა: "))
+#
+#
+# if score >= 91 and score <= 100:
+#     print("A")
+# elif score >= 81 and score <= 90:
+#     print("B")
+# elif score >= 71 and score <= 80:
+#     print("C")
+# elif score >= 61 and score <= 70:
+#     print("D")
+# elif score >= 51 and score <= 60:
+#     print("E")
+# elif score > 100:
+#     print("wrong score")
+# elif score < 0:
+#     print("wrong score")
+# else:
+#     print("F")
+
+# 2.
+#
+# numb = int(input("შეიყვანეთ რიცხვი"))
+#
+# if numb % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# def print_first_word(input_str):
+#     words = input_str.split()
+#     if "hi" in words or "bye" in words or "hello" in words:
+#         print(words[])
+#
+# input_str = input("Enter a sentence: ")
+# print_first_word(input_str)
+
+def find_words(text):
+    words = text.split()
+    elif "small" in words:
+        print("small")
+    elif "tall" in words:
+        print("tall")
+    elif "middle" in words:
+        print("middle")
+    else:
+        print("ტექსტში ეს სამი სიტყვა არ მოიძებნა")
+
+text = input("შემოიტანეთ ტექსტი: ")
+find_words(text)
+
